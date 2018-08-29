f48fa1291a3411ce7a440975753818851ba9d781
4015c13 <4015c13@tamarindo.ece.uprm.edu>
2018-08-29 12:26:59 -0400

    seniority has been established

--- src/Employee.java
+++ src/Employee.java
@@ -81,8 +81,13 @@ public class Employee {
 	public int seniorityLevel() {
 		
 		//Add Code Here [You have to use If/Else to get graded]
-		
+		if (yearService>= 1 && yearService < 5){
+			return 1;
+			}else if(yearService>= 5 && yearService < 10){
+				return 2;
+			}else if (yearService>= 10){
+				return 3;
+			}
 		return 0;// Temporal Return
-		
+		}
 	}
-}
